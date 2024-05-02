add67d10b8e49b7af3a955dc7020c86b70e04ed9
techysj <techysj0505@gmail.com>
2024-05-03 01:13:11 +0530

    Fix for api

--- app/api/promt/route.js
+++ app/api/promt/route.js
@@ -1,6 +1,8 @@
 import Prompt from "@models/prompt";
 import { connectDB } from "@utils/database";
 
+export const revalidate = 0;
+
 export const GET = async (request) => {
   try {
     await connectDB();
